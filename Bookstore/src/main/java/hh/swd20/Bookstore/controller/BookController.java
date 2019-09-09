271fdb57874d90f02d2ea168e8233e6267888693
Halfeen <anna1.kroger@gmail.com>
2019-09-09 12:01:25 +0300

    booklist lisätty

--- Bookstore/src/main/java/hh/swd20/Bookstore/controller/BookController.java
+++ Bookstore/src/main/java/hh/swd20/Bookstore/controller/BookController.java
@@ -1,10 +1,14 @@
 package hh.swd20.Bookstore.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import hh.swd20.Bookstore.domain.Book;
 import hh.swd20.Bookstore.domain.BookRepository;
 
 //Add a new controller called BookController which handle get request to the path /index 
@@ -18,6 +22,13 @@ public class BookController {
 	public String Books() {
 	
 	return "welcome";
+	}
+	
+	@RequestMapping(value = "/booklist", method = RequestMethod.GET)
+	public String getBooks(Model model) {
+		List<Book> books = (List<Book>) bookRepository.findAll();
+		model.addAttribute("books", books);
+		return "booklist";
 	}
 	
 }
